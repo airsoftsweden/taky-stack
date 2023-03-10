062e73f45764b6e022224d2fbc07a16bdbbb8244
Christoffer Claesson <Christoffer.Claesson@securitybits.io>
2023-03-10 14:57:28 +0100

    Initial Working commit

---
+++ terraform/taky-droplet.tf
@@ -0,0 +1,71 @@
+resource "digitalocean_droplet" "atak-docker-do" {
+  count = length(var.servers)
+  image = "ubuntu-20-04-x64"
+  name = "taky-docker-do-${var.servers[count.index]}"
+  region = "ams3"
+  size = "s-1vcpu-1gb"
+  ssh_keys = [ var.ssh_fingerprint ]
+  
+  connection {
+    host  = self.ipv4_address
+    user  = "root"
+    type = "ssh"
+    private_key = file(var.ssh_key)
+    timeout = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "apt-add-repository -y ppa:ansible/ansible",
+      "apt update",
+      "apt install -y git",
+      "apt install -y ansible"
+    ]
+  }
+
+  provisioner "file" {
+    source = "../secrets"
+    destination = "/opt/secrets"
+  }
+
+  provisioner "file" {
+    source = "../ansible"
+    destination = "/opt/ansible"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cd /root",
+      "echo 'export TAKY_SERVER=${var.servers[count.index]}' >> .bash_profile",
+      "echo 'export IP=${var.servers[count.index]}.airsoftsweden.com' >> .bash_profile",
+      "echo 'export ID=ATAK-${var.servers[count.index]}' >> .bash_profile",
+      "echo 'export KEY_PW=${var.cert_pass}' >> .bash_profile",
+      "echo 'export SERVER_P12_PW=${var.cert_pass}' >> .bash_profile"
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export TAKY_SERVER=${var.servers[count.index]}",
+      "export IP=${var.servers[count.index]}.airsoftsweden.com",
+      "export ID=ATAK-${var.servers[count.index]}",
+      "export KEY_PW=${var.cert_pass}",
+      "export SERVER_P12_PW=${var.cert_pass}",
+      "cd /opt/ansible",
+      "ansible-galaxy collection install -r requirements.yml",
+      "ansible-playbook -i ansible_hosts taky.yml"
+    ]
+  }
+}
+
+resource "namecheap_domain_records" "milsim-airsoftsweden" {
+  domain = "airsoftsweden.com"
+  mode = "MERGE"
+  count = length(var.servers)
+
+  record {
+    hostname = "${var.servers[count.index]}"
+    type = "A"
+    address = digitalocean_droplet.atak-docker-do[count.index].ipv4_address
+  }
+}
---
+++ terraform/vars.tf
@@ -0,0 +1,41 @@
+variable "do_token" {
+  type        = string
+  sensitive   = true
+}
+
+variable "ssh_key" {
+  default = "~/.ssh/id_rsa"
+  type = string
+}
+
+variable "gh_token" {
+  default = "NONE"
+  type = string  
+}
+
+variable "cert_pass" {
+  default = "atakatak"
+  type = string
+}
+
+variable "ssh_fingerprint" {
+  type = string  
+}
+
+variable "namecheap_username" {
+  type = string
+}
+
+variable "namecheap_api_user" {
+  type = string
+}
+
+variable "namecheap_api_key" {
+  type = string
+}
+
+variable "servers" {
+  description = "Create Servers with these names"
+  type = list(string)
+}
+
